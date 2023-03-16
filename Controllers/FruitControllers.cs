85e2f2d6ad6bebf684622324f4d95801b453bfc3
mateus <mateusbona28@gmail.com>
2023-03-16 11:31:16 -0300

    feat: rotas de get/post finalizadas e prontas para testes

--- Controllers/FruitControllers.cs
+++ Controllers/FruitControllers.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Fruit.Model;
+using Fruit.Repository.Interface;
 
 namespace Fruit.Controllers {
     
@@ -7,20 +8,28 @@ namespace Fruit.Controllers {
     [ApiController]
 
     public class FruitController : ControllerBase {
+        private readonly IFruitRepository _fruitsRepository;
+        public FruitController(IFruitRepository fruitsRepository)
+        {
+            _fruitsRepository = fruitsRepository;
+        }
 
         [HttpGet]
-        public ActionResult<List<FruitModel>> GetFruits() {
-            return Ok();
+        public async Task<ActionResult<List<FruitModel>>> GetFruits() {
+            List<FruitModel> fruits = await _fruitsRepository.ListAllFruits();
+            return Ok(fruits);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<FruitModel> GetFruitById() {
-            return Ok();
+        public async Task<ActionResult<FruitModel>> GetFruitById(int id) {
+            FruitModel fruit = await _fruitsRepository.ListFruitById(id);
+            return Ok(fruit);
         }
 
         [HttpPost]
-        public ActionResult<FruitModel> PostFruit() {
-            return Ok();
+        public async Task<ActionResult<FruitModel>> PostFruit([FromBody]FruitModel fruit) {
+            FruitModel createdFruit = await _fruitsRepository.CreateNewFruit(fruit);
+            return Ok(createdFruit);
         }
     }
 }
