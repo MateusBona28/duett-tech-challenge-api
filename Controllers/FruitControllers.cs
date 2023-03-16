5000bc373f038eaa80067967d4f9f3ff31738a18
mateus <mateusbona28@gmail.com>
2023-03-16 09:43:36 -0300

    feat: adicionando base das rotas get/post na aplicação

--- Controllers/FruitControllers.cs
+++ Controllers/FruitControllers.cs
@@ -1,5 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Fruit.Model;
 
 namespace Fruit.Controllers {
     
+    [Route("api/[Controller]")]
+    [ApiController]
+
+    public class FruitController : ControllerBase {
+
+        [HttpGet]
+        public ActionResult<List<FruitModel>> GetFruits() {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FruitModel> GetFruitById() {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<FruitModel> PostFruit() {
+            return Ok();
+        }
+    }
 }
